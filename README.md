41f7c65861c6af782436dafa24e7756949a0cf87
Adnan Sahinovic <a.sahinovic@yahoo.com>
2020-01-01 23:25:55 +0700

    Adding readme file and changing funtion exports

---
+++ README.md
@@ -0,0 +1,64 @@
+# excel-upload
+
+A node.js module for uploading and converting excel files into JSON.
+
+### Installing
+
+```
+npm install excel-upload
+```
+
+### Usage
+
+Excel-upload works on top of the multer. Uploading, storing and parsing files is done
+asynchronous and the result is returned as a Promise.
+
+Don't forget the enctype="multipart/form-data" in your form.
+
+**Browser:**
+
+```
+<form action="/upload-documents" method="post" enctype="multipart/form-data">
+  <input type="file" name="upload-documents" />
+</form>
+
+```
+
+**Controller:**
+
+```
+import uploadFile from 'excel-upload';
+  
+static async post(req, res, next) {
+ 
+    const upload = await uploadFile(req, "my-document", "", "upload-documents");
+ 
+      res.json(upload);
+  }
+```
+
+## API
+
+| Key | Description |
+| --- | --- |
+| req | req express.js object |
+| fileName | name of the uploaded file|
+| fileLocation | location where uploaded file is stored |
+| fieldName | field name specified in the form |
+
+
+## Built With
+
+* [Node](https://nodejs.org/en/) - Node.js 
+* [Multer](https://www.npmjs.com/package/multer) - Node.js module for upload files
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+
--- index.js
+++ index.js
@@ -3,66 +3,62 @@ const multer = require('multer');
 const path = require('path');
 const util = require('util');
 
-class UploadExcel {
-	static async uploadFile(req, fileName, fileLocation, fieldName) {
-		const startUpload = await this.prepareFileUpload(fileName, fileLocation, fieldName);
+exports = module.exports = uploadFile;
 
+/** Initialize multer and upload file */
+async function uploadFile(req, fileName, fileLocation, fieldName) {
+	const startUpload = await prepareFileUpload(fileName, fileLocation, fieldName);
 
-		await startUpload(req, fileName);
+	await startUpload(req, fileName);
 
-		const { path: filePath } = req.file;
-		const { path: routePath } = req.route;
-		const converted = await this.convertExcelToJson(filePath, routePath);
-		return converted;
-	}
-
-	/** Multer disk storage settings, file naming and storing location */
-	static async prepareFileUpload(fileName, folderName, fieldName) {
-		const uploadedExcelFilePath = path.join(__dirname + folderName);
-		const storage = multer.diskStorage({
-			destination: (req, file, cb) => {
-				cb(null, uploadedExcelFilePath);
-			},
-			filename: (req, file, cb) => {
-				const { originalname } = file;
-				const uploadedExcelFileDate = Date.now();
-				const uploadedExcelFileName = `${fileName}-${uploadedExcelFileDate}.${originalname.split('.')[
-					originalname.split('.').length - 1
-				]}`;
-				if ([ 'xls', 'xlsx' ].indexOf(originalname.split('.')[originalname.split('.').length - 1]) === -1) {
-					return cb('Wrong file type (it should be .xls or .xlsx');
-				}
-				cb(null, uploadedExcelFileName);
-			}
-		});
-		const uploadedFile = util.promisify(
-			multer({
-				storage: storage
-			}).single(fieldName)
-		);
-		return uploadedFile;
-	}
-
-	static convertExcelToJson(filePath) {
-		return new Promise((resolve, reject) => {
-			xlsxj(
-				{
-					input: filePath,
-					output: null
-				},
-				(error, file) => {
-					if (error) {
-						reject(new Error('Error while converting file from xlss to json'));
-					} else {
-						console.log('USO SAAM DA CONVERTUJEM 2', file);
-						resolve(file);
-					}
-				}
-			);
-		});
-	}
+	const { path: filePath } = req.file;
+	const { path: routePath } = req.route;
+	const converted = await convertExcelToJson(filePath, routePath);
+	return converted;
 }
 
-module.exports = {
-	UploadExcel
-};
+/** Multer disk storage settings, file naming and storing location */
+function prepareFileUpload(fileName, folderName, fieldName) {
+	const uploadedExcelFilePath = path.join(__dirname + folderName);
+	const storage = multer.diskStorage({
+		destination: (req, file, cb) => {
+			cb(null, uploadedExcelFilePath);
+		},
+		filename: (req, file, cb) => {
+			const { originalname } = file;
+			const uploadedExcelFileDate = Date.now();
+			const uploadedExcelFileName = `${fileName}-${uploadedExcelFileDate}.${originalname.split('.')[
+				originalname.split('.').length - 1
+			]}`;
+			if ([ 'xls', 'xlsx' ].indexOf(originalname.split('.')[originalname.split('.').length - 1]) === -1) {
+				return cb('Wrong file type (it should be .xls or .xlsx');
+			}
+			cb(null, uploadedExcelFileName);
+		}
+	});
+	const uploadedFile = util.promisify(
+		multer({
+			storage: storage
+		}).single(fieldName)
+	);
+	return uploadedFile;
+}
+
+/** Converting xls to json and returning promise with result */
+function convertExcelToJson(filePath) {
+	return new Promise((resolve, reject) => {
+		xlsxj(
+			{
+				input: filePath,
+				output: null
+			},
+			(error, file) => {
+				if (error) {
+					reject(new Error('Error while converting file from xlss to json'));
+				} else {
+					resolve(file);
+				}
+			}
+		);
+	});
+}
